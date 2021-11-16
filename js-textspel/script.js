fe8051dd15c6b075a601b020c15ced611813334c
PettoDavida <maximilian.widman@gmail.com>
2021-11-16 15:43:20 +0100

    Pick up weapons kill ogre and villager speak

--- js-textspel/script.js
+++ js-textspel/script.js
@@ -11,9 +11,10 @@ const swamp = 'S';
 const gate = 'G';
 const wall = 'W';
 let sword = false;
+let axe = false;
 let gateKey = false;
 let talkToChief = false;
-let ogre = {seen: false, hp: 100,};
+let ogre = { hp: 100, dead: false};
 
 let override = false;
 let story = document.querySelector("#story");
@@ -75,10 +76,58 @@ function action(){
             x--
         }
     }if(action == "hint"){
-        alert("To move write move north, south, east or west. \nTo pick up an item write pickup (itemname)");
+        if (y == 5 && x == 1) {
+            alert("To move write move north, south, east or west. \nTo pick up an item write pickup (itemname)");
+        }if (y == 3 && x == 8) {
+            alert("To move write move north, south, east or west. \nTo hit Neo the Ogre write hit ogre with (weaponname) or just hit ogre");
+        }else{
+            alert("To move write move north, south, east or west.");
+        }
+    }if (action == "pickup sword") {
+        if (y == 5 && x == 1) {
+            sword=true;
+        } else {
+            return;
+        }
+    }if (action == "pickup axe") {
+        if (y == 5 && x == 1) {
+            axe=true;
+        } else {
+            return;
+        }
+    }if (action == "hit ogre") {
+        if (y == 3 && x == 8) {
+            let weapon = prompt("with what?")
+            if (weapon == "sword") {
+                hitOgre(15)
+            }if (weapon == "axe") {
+                hitOgre(25)
+            }
+        }
+    
+    }if (action == "hit ogre with sword") {
+        if (y == 3 && x == 8) {
+            hitOgre(15)
+        }
+    
+    }if (action == "hit ogre with axe") {
+        if (y == 3 && x == 8) {
+            hitOgre(25)
+        }
+    
     }
-
 }
+
+function hitOgre(value) {
+    ogre.hp -= value;
+            if (ogre.hp <= 0) {
+                story.innerHTML = ("The ogre has died.<br> You see a key hanging from the dead ogres belt.")
+                ogre.dead = true;
+            }else{
+            story.innerHTML = ("The ogre has " + ogre.hp.toString() + "HP left" )
+        }
+}
+
 
 function checkSquare(){
     if (override) {
@@ -145,37 +194,62 @@ function blacksmith(){
     if (!talkToChief) {
         if (!talkToBlacksmith) {
             story.innerHTML= "Hello stranger, I am the village blacksmith and you should talk to the village chief. Until you have I can't help you.";
-            talkToBlacksmith = true;
         }else{
             story.innerHTML= "You again?! I told you to talk to the village chief now bugger off I have work to do."
         }
-    }
-    else{
+    }else{
         if (talkToBlacksmith) {
-            story.innerHTML = "I heard you talked to the chief and he told me to help you with whatever you need";
+            story.innerHTML = "I heard you talked to the chief and he told me to get out my best weapons so that you could help slay Neo the Ogre.<br> What weapon would you like?<br> *on the table you find a pristine looking sword and a battle hardened battle-axe*";
         } else {
-            story.innerHTML = "Hello stranger, I am the village blacksmith the chief told me to help you with whatever you need";
-            talkToBlacksmith=true;
+            story.innerHTML = "Stranger! I've been expecting you.<br> The chief told me to get out my best weapons so that you could help slay Neo the Ogre.<br> What weapon would you like?<br> *on the table you find a pristine looking sword and a battle hardened battle-axe*";
         }
     }
+
+
+    talkToBlacksmith = true;
 }
+
 function chief(){
-    if (talkToChief && talkToBlacksmith) {
-        story.innerHTML = 
+    if (talkToChief) {
+        if (talkToBlacksmith) {
+            story.innerHTML = "Come on go kill Neo the Ogre.";
+        }else{
+            story.innerHTML = "You should go talk to the blacksmith. I have told him to expect you.";
+        }
+    }else{
+        if (talkToBlacksmith) {
+            story.innerHTML = "Hello, I've been waiting the blacksmith told me to expect a stranger to come by. ";
+        }else{
+            story.innerHTML = "Welcome to our village stranger. As you may have seen our village has been abandoned. <br> This is because of Neo the Ogre that lives in the swamp to the west. If you would be willing to help I will have a message delivered to the blacksmith telling him to expect you.<br> He should be able to give you some sort of weapon.";
+        }
     }
-    if (!talkToChief) {
-        story.innerHTML= "Hello stranger, welcome to our village. What brings you to our village?";
-        talkToChief = true;
-    }if (talkToBlacksmith) {
-        story.innerHTML = "Hello, I've been waiting the blacksmith told me to expect a stranger to come by. ";
+    talkToChief = true;
+}
+
+function getWeapon(){
+
+    if (sword && axe) {
+        return "sword and axe";    
+    }if (sword) {
+        return "sword";
+    }if (axe) {
+        return "axe";
     }
 }
 
 function neoTheOgre(){
-    story.innerHTML= "";
+    if (talkToChief) {
+        if (sword || axe) {
+            story.innerHTML= "As you approach Neo the hulking ogre you draw your " + getWeapon() + " getting ready to strike";
+        }else{
+            story.innerHTML= "You see Neo the hulking ogre you should probably go the other way";
+        }
+    }else{
+        story.innerHTML = "You see a hulking ogre you should probably go the other way"
+    }
+    
 }
 
 function wallGate(){
     story.innerHTML= "You have come upon a giant gate with walls stretching as far as the eye can see to the west and east";
-
 }
