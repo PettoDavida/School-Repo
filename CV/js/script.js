6541f05e71a31723c3f794c318e9fce9e570c60c
PettoDavida <maximilian.widman@gmail.com>
2021-11-30 17:56:17 +0100

    styled a tag + nav list  + added interactive js background

--- CV/js/script.js
+++ CV/js/script.js
@@ -1,4 +1,3 @@
-const body = document.body;
 const nav = document.querySelector('nav');
 const fixed = 'fixed';
 let lastScroll = 0;
@@ -19,3 +18,40 @@ window.addEventListener('scroll', () => {
     }
     lastScroll = currentScroll;
 });
+
+let url = document.location.pathname;
+
+let om_mig = document.querySelector('#om-mig');
+let intressen = document.querySelector('#intressen');
+let erfarenheter = document.querySelector('#erfarenheter');
+
+
+switch (url) {
+    case "/index.html":
+        om_mig.style.borderColor = "white";
+        break;
+
+    case "/erfarenheter.html":
+        erfarenheter.style.borderColor = "white";
+        break;
+
+    case "/intressen.html":
+        intressen.style.borderColor = "white";
+        break;
+            
+    default:
+        break;
+}
+
+
+
+VANTA.BIRDS({
+    el: "main",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
