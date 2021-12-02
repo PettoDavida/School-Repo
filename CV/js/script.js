f4f8a996e2f784a584ef7fc074493c715648d43a
PettoDavida <maximilian.widman@gmail.com>
2021-12-02 15:17:26 +0100

    erfarenheter done ytplayer size depend on screen done

--- CV/js/script.js
+++ CV/js/script.js
@@ -1,14 +1,15 @@
 
-// makes navbar visible when you scroll up by comparing y positions of where you are and where you were. 
-// if y becomes smaller you will see the navbar and if y becomes bigger it will disappear
 const nav = document.querySelector('nav');
 const fixed = 'fixed';
+const url = document.location.pathname;
 /**
  * location of lastscroll
  */
 let lastScroll = 0;
 
-window.addEventListener('scroll', () => {
+// makes navbar visible when you scroll up by comparing y positions of where you are and where you were. 
+// if y becomes smaller you will see the navbar and if y becomes bigger it will disappear
+function scrollNav() {
     const currentScroll = window.pageYOffset;
     if (currentScroll <= 0) {
         nav.classList.remove(fixed);
@@ -23,6 +24,10 @@ window.addEventListener('scroll', () => {
         nav.classList.add(fixed);        
     }
     lastScroll = currentScroll;
+}
+
+window.addEventListener('scroll', () => {
+    scrollNav();
 });
 
 
@@ -30,8 +35,7 @@ window.addEventListener('scroll', () => {
  * Makes it easier to see where you are on the site by giving the navbar item a white border on the bottom
  */
 function navbarLocation() {
-    let url = document.location.pathname;
-
+    
     let om_mig = document.querySelector('#om-mig');
     let intressen = document.querySelector('#intressen');
     let erfarenheter = document.querySelector('#erfarenheter');
@@ -74,21 +78,18 @@ function birdBackground() {
       })
 }
 
-window.addEventListener('load', (event) => {
-    birdBackground();
-    navbarLocation();
-  });
-
-
-  let vid1 = document.querySelector('#video1');
   
-
-  function playVid() {
-      vid1.play();
+/**
+ * plays video
+ */
+  function playVid(vid) {
+      vid.play();
   }
-
-  function pauseVid() {
-      vid1.pause();
+/**
+ * pauses video
+ */
+  function pauseVid(vid) {
+      vid.pause();
   }
 
   Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
@@ -96,12 +97,26 @@ window.addEventListener('load', (event) => {
         return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
     }
     })
-
-  function vidControls() {
-      if (vid1.playing) {
-          pauseVid();
+/**
+ * makes video playable and pauseable by just clicking on the video 
+ */
+  function vidControls(video) {
+    let vidElement = document.querySelector(video);
+      if (vidElement.playing) {
+          pauseVid(vidElement);
       } else {
-          playVid();
+          playVid(vidElement);
       }
   }
 
+
+
+  window.addEventListener('load', (event) => {
+    scrollNav();
+    navbarLocation();
+
+    if (url !== "/erfarenheter.html") {
+        birdBackground();
+    } 
+    
+  });
