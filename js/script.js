e559d34b29bc2da4a7d35203c63e54aeb3de07e7
PettoDavida <maximilian.widman@gmail.com>
2021-12-03 11:11:17 +0100

    cant  find js for some reason

--- js/script.js
+++ js/script.js
@@ -42,15 +42,15 @@ function navbarLocation() {
 
 
     switch (url) {
-        case "./index.html":
+        case "/index.html":
             om_mig.style.borderColor = "white";
             break;
 
-        case "./erfarenheter.html":
+        case "/erfarenheter.html":
             erfarenheter.style.borderColor = "white";
             break;
 
-        case "./intressen.html":
+        case "/intressen.html":
             intressen.style.borderColor = "white";
             break;
 
